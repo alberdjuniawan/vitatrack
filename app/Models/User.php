63f30e00d09624777388a581b6e2d1a0c76271b5
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-04 18:38:14 +0700

    feat: add health logs and insight relation, add existing_coditions and needs weekly assessment

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,46 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Concerns\HasUlids; 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUlids; 
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'date_of_birth',
+        'gender',
+        'height_cm',
+        'weight_kg',
+        'existing_conditions',
+        'timezone',
+        'needs_weekly_assessment',
+    ];
+    protected $hidden = ['password', 'remember_token'];
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'existing_conditions' => 'array',
+            'needs_weekly_assessment' => 'boolean',
         ];
+    }
+
+    public function healthLogs()
+    {
+        return $this->hasMany(HealthLog::class);
+    }
+
+    public function insights()
+    {
+        return $this->hasMany(Insight::class);
     }
 }
