4a6b1c88ce6e8d20545db6dfc277e5858c945514
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-04 18:29:40 +0700

    feat: add needs weekly asignment to user table migration

---
+++ database/migrations/2026_05_04_171152_add_needs_weekly_assessment_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('needs_weekly_assessment')->default(true)->after('timezone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('needs_weekly_assessment');
+        });
+    }
+};
