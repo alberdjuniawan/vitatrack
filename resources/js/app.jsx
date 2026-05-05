b7ba5f1443300c0e774696a0b1785fa447bd36b9
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-05 19:08:43 +0700

    feat: init react app entry point

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import '../css/app.css';
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = document.getElementById('root');
+if (root) {
+    ReactDOM.createRoot(root).render(<App />);
+}
